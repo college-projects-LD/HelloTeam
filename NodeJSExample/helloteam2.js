d87abb04b539c61c5beb4fd00b086aeb7657b9e8
Brian <bpayne22@byui.edu>
2023-05-04 19:36:46 -0700

    Added something aswell

--- NodeJSExample/helloteam2.js
+++ NodeJSExample/helloteam2.js
@@ -1,4 +1,4 @@
-const helloTeam = ["matthew", "Brian", "Pun", "Grant", "Ryan", "Samantha","Landon"];
+const helloTeam = ["matthew", "Brian_Thats_Me", "Pun", "Grant", "Ryan", "Samantha","Landon"];
 
 names.forEach((item, index) => {
     console.log(item, index);
